7c607330f346a7214a88e73c49f80e4308860cce
rafen <capdevielle@gmail.com>
2017-06-30 12:36:58 -0300

    Fixed import order for js

--- gulpfile.js
+++ gulpfile.js
@@ -3,60 +3,60 @@
 
 // Call Plugins
 var env        = require('minimist')(process.argv.slice(2)),
-	gulp       = require('gulp'),
-	jade       = require('gulp-jade'),
-	uglify     = require('gulp-uglify'),
-	compass    = require('gulp-compass'),
-	concat     = require('gulp-concat'),
-	cssmin     = require('gulp-cssmin'),
-	gulpif     = require('gulp-if'),
-	connect    = require('gulp-connect'),
-	modRewrite = require('connect-modrewrite'),
-	imagemin   = require('gulp-imagemin');
+    gulp       = require('gulp'),
+    jade       = require('gulp-jade'),
+    uglify     = require('gulp-uglify'),
+    compass    = require('gulp-compass'),
+    concat     = require('gulp-concat'),
+    cssmin     = require('gulp-cssmin'),
+    gulpif     = require('gulp-if'),
+    connect    = require('gulp-connect'),
+    modRewrite = require('connect-modrewrite'),
+    imagemin   = require('gulp-imagemin');
 
 // Collect template
 gulp.task('template', function(){
-	return gulp.src('src/templates/*.html')
-		.pipe(gulp.dest('build/'))
-		.pipe(connect.reload());
+    return gulp.src('src/templates/*.html')
+        .pipe(gulp.dest('build/'))
+        .pipe(connect.reload());
 });
 
 // Collect fonts
 gulp.task('fonts', function(){
-	return gulp.src('src/fonts/**/*')
-		.pipe(gulp.dest('build/fonts/'))
-		.pipe(connect.reload());
+    return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('build/fonts/'))
+        .pipe(connect.reload());
 });
 
 // Call Jade for compile Templates
 gulp.task('jade', function(){
-	return gulp.src('src/templates/*.jade')
-		.pipe(jade({pretty: !env.p }))
-		.pipe(gulp.dest('build/'))
-		.pipe(connect.reload());
+    return gulp.src('src/templates/*.jade')
+        .pipe(jade({pretty: !env.p }))
+        .pipe(gulp.dest('build/'))
+        .pipe(connect.reload());
 });
 
 // Call Uglify and Concat JS
 gulp.task('js', function(){
-	return gulp.src('src/js/**/*.js')
-		.pipe(concat('main.js'))
-		.pipe(gulpif(env.p, uglify()))
-		.pipe(gulp.dest('build/js/'))
-		.pipe(connect.reload());
+    return gulp.src(['src/js/bootstrap/tooltip.js', 'src/js/**/*.js'])
+        .pipe(concat('main.js'))
+        .pipe(gulpif(env.p, uglify()))
+        .pipe(gulp.dest('build/js/'))
+        .pipe(connect.reload());
 });
 
 // Call Sass
 gulp.task('compass', function(){
-	return gulp.src('src/sass/main.scss')
-		.pipe(compass({
-			css: 'src/css',
-			sass: 'src/sass',
-			image: 'src/img'
+    return gulp.src('src/sass/main.scss')
+        .pipe(compass({
+            css: 'src/css',
+            sass: 'src/sass',
+            image: 'src/img'
 
-		}))
-		.pipe(gulpif(env.p, cssmin()))
-		.pipe(gulp.dest('build/css/'))
-		.pipe(connect.reload());
+        }))
+        .pipe(gulpif(env.p, cssmin()))
+        .pipe(gulp.dest('build/css/'))
+        .pipe(connect.reload());
 });
 
 // Call Imagemin
@@ -69,27 +69,27 @@ gulp.task('imagemin', function() {
 
 // Call Watch
 gulp.task('watch', function(){
-	gulp.watch('src/templates/**/*.html', ['template']);
-	// gulp.watch('src/templates/**/*.jade', ['jade']);
-	gulp.watch('src/sass/**/*.scss', ['compass']);
-	gulp.watch('src/js/**/*.js', ['js']);
-	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
+    gulp.watch('src/templates/**/*.html', ['template']);
+    // gulp.watch('src/templates/**/*.jade', ['jade']);
+    gulp.watch('src/sass/**/*.scss', ['compass']);
+    gulp.watch('src/js/**/*.js', ['js']);
+    gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
 });
 
 // Connect (Livereload)
 gulp.task('connect', function() {
-	connect.server({
-		root: ['build/'],
-		livereload: true,
-		middleware: function(){
-			return [
-				modRewrite([
-					'^/$ /index.html',
-					'^([^\\.]+)$ $1.html'
-				])
-			];
-		}
-	});
+    connect.server({
+        root: ['build/'],
+        livereload: true,
+        middleware: function(){
+            return [
+                modRewrite([
+                    '^/$ /index.html',
+                    '^([^\\.]+)$ $1.html'
+                ])
+            ];
+        }
+    });
 });
 
 // Default task
